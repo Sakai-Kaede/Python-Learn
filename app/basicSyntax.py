ebf5ca8badd1767093db00f2cbd7ec770315b29e
Sakai-Kaede <kana1024k@gmail.com>
2024-07-20 23:58:42 +0900

    Learned about in, loops, and exception handling

--- app/basicSyntax.py
+++ app/basicSyntax.py
@@ -12,5 +12,66 @@ def first_item(items):
   else:
     return None
   
-print(first_item(['book'])) # bookと表示される
-print(first_item([])) # Noneと表示される
+print(first_item(['book'])) # [book]と表示される
+print(first_item([])) # [None]と表示される
+
+# ある要素がコンテナオブジェクトに含まれるか判定
+count = {'book': 1, 'note': 2}
+print('book' in count) # [True]と表示される
+
+
+### ループについて ###
+
+# for文は変数のスコープをブロック内に限定しない
+i = 100
+for i in range(3):
+  pass
+
+print(i) # 2と表示される
+
+# ↑ ループで使う変数名を(_)で統一しておくと、変数名が被ること問題を軽減できる
+
+
+### 例外処理について ###
+
+# try文を利用すると強制終了を避けられる
+
+def get_book(index):
+  items = ['note', 'notebook', 'sketchbook']
+  try:
+    return items[index]
+  except IndexError:
+    return '範囲外です'
+  
+get_book(10) # [範囲外です]と表示される
+
+## 補足 ## except節は複数記述できる
+
+# else節を使ってtryブロックを小さくすることで
+# 例外が発生する可能性のある処理が明確になる
+
+def get_book_upper(index):
+  items = ['note', 'notebook', 'sketchbook']
+  try:
+    book = str(items[index])
+    # return book.upper() else節へ移動する
+  except (IndexError, TypeError) as e:
+    print(f'例外が発生しました: {e}')
+  else:
+    return book.upper()
+
+# finally節は例外の発生有無にかかわらずtryブロックを
+# 抜ける際に必ず実行される
+
+f = open('some.txt', 'r')
+try:
+  print(f.read())
+finally:
+  f.close()
+
+# with文は事前に定義されているクリーンアップ処理を利用する
+
+with open('some.txt', 'w') as f:
+  f.write('some text')
+
+print(f.closed)
