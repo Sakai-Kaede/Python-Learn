dbb75504e2e96b27750c5bc82bf4b17c151acc30
Sakai-Kaede <kana1024k@gmail.com>
2024-07-22 23:58:56 +0900

    Learned about adding and deleting elements of type list

--- app/dataStructure.py
+++ app/dataStructure.py
@@ -9,3 +9,22 @@ if n is None:
 else:
   print('nの値はNoneではありません')
 # [nの値はNoneです]と表示される
+
+
+### 配列について ###
+
+# list型
+
+# 要素の追加と削除
+items = ['note', 'notebook', 'sketchbook']
+items.append('paperbook')
+print(items) # ['note', 'notebook', 'sketchbook', 'paperbook']と表示される
+
+items = ['book'] + items
+print(items) # ['book', 'note', 'notebook', 'sketchbook', 'paperbook']と表示される
+
+items.pop(0)
+print(items) # ['note', 'notebook', 'sketchbook', 'paperbook']と表示される
+
+del items[1]
+print(items)# ['note', 'sketchbook', 'paperbook']と表示される
